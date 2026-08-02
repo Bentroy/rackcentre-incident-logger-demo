d563786d379ca6fc8184695634f0a0047ea6d49d
Benjamin David <bentroy006@gmail.com>
2026-08-02 17:50:27 +0100

    Update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,10 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
     credentials: true,
   })
 );
